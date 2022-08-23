39ffa5e47f80d977f508730feb5376a35b21e979
Home <david.klesch@gmail.com>
2022-08-23 21:41:36 +0300

    23.08 commit

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,11 @@
-package hexlet.code ;
+package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+    }
+    public static String whatIsYourName(String str) {
+        System.out.println("May I have your name?");
+        return "Hello, " + str + "!";
     }
 }
