11370d1a84e5ac4e1daf8e657cfe01517fb37639
Home <david.klesch@gmail.com>
2022-08-26 22:42:07 +0300

    26.08 5 step

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,10 +2,19 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
     }
-    public static String whatIsYourName(String str) {
+    public static boolean start(int i) {
+        if (i == 1) {
+            System.out.println("Welcome to the Brain Games!");
+        } return true;
+    }
+    public static String whatIsYourName(String name) {
         System.out.println("May I have your name?");
-        return "Hello, " + str + "!";
+            return "May I have your name? " + " " + name;
+
     }
-}
+
+    }
