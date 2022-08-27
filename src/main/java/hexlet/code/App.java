de6d991d6ece207e6503fa0efe2b19e1b6f3682b
Home <david.klesch@gmail.com>
2022-08-27 20:09:02 +0300

    27.08 5 step

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,20 +1,66 @@
 package hexlet.code;
+import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
+        Scanner sc = new Scanner(System.in);
+        int gameNumber = sc.nextInt();
+        start(1);
     }
     public static boolean start(int i) {
         if (i == 1) {
             System.out.println("Welcome to the Brain Games!");
-        } return true;
+            System.out.println("May I have your name?");
+            Scanner sc = new Scanner(System.in);
+            String name = sc.nextLine();
+            whatIsYourName(name);
+            return true;
+        } return false;
     }
     public static String whatIsYourName(String name) {
-        System.out.println("May I have your name?");
-            return "May I have your name? " + " " + name;
-
+        for (var i = 0; i < 3; i++) {
+        int rand = 1;
+        System.out.println("Hello, " + name);
+        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+        System.out.println("Question: " + rand);
+        System.out.println("Your answer: ");
+        Scanner sc = new Scanner(System.in);
+        String yesNo = sc.nextLine();
+        if (isNumberParity(yesNo) == false) {
+            break;
+        }
+        isNumberParity(yesNo);
+        if (i == 2) {
+            System.out.println("Congratulations, " + name + "!");
+        }
+        }
+        return "Hello, " + name;
     }
+    public static boolean isNumberParity(String yesNo) {
 
+        int rand = 1;
+            if ((rand % 2 == 0)&(yesNo.equals("yes"))) {
+                System.out.println("Correct!");
+            }
+            if ((rand % 2 != 0)&(yesNo.equals("yes"))) {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                System.out.println("Let's try again, " + "!");
+                return false;
+        }
+            if ((rand % 2 == 0)&(yesNo.equals("no"))) {
+                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
+                System.out.println("Let's try again, "  + "!");
+                return false;
+        } if ((rand % 2 != 0)&yesNo.equals("no")) {
+                System.out.println("Correct!");
+        } else {
+                System.out.println("Try again!");
+                return false;
+            } return true;
+    }
     }
