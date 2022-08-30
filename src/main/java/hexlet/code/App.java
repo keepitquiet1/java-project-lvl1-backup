3b5be57b1d90d8375983fba73b7c3fe72b666e12
Home <david.klesch@gmail.com>
2022-08-30 19:32:43 +0300

    30.08

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,66 +1,73 @@
 package hexlet.code;
-import java.util.Random;
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
+        if (!start()) return;
+        whatIsYourName();
+        String yN = isNumberParity();
+        playGame(yN);
+
+
+
+    }
+
+    public static boolean start() {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("0 - Exit");
         System.out.println("Your choice: ");
         Scanner sc = new Scanner(System.in);
         int gameNumber = sc.nextInt();
-        start(1);
+        if (gameNumber != 1) return false;
+        return true;
     }
-    public static boolean start(int i) {
-        if (i == 1) {
-            System.out.println("Welcome to the Brain Games!");
-            System.out.println("May I have your name?");
-            Scanner sc = new Scanner(System.in);
-            String name = sc.nextLine();
-            whatIsYourName(name);
-            return true;
-        } return false;
-    }
-    public static String whatIsYourName(String name) {
-        for (var i = 0; i < 3; i++) {
-        int rand = 1;
-        System.out.println("Hello, " + name);
-        System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
-        System.out.println("Question: " + rand);
-        System.out.println("Your answer: ");
-        Scanner sc = new Scanner(System.in);
-        String yesNo = sc.nextLine();
-        if (isNumberParity(yesNo) == false) {
-            break;
-        }
-        isNumberParity(yesNo);
-        if (i == 2) {
-            System.out.println("Congratulations, " + name + "!");
-        }
-        }
-        return "Hello, " + name;
-    }
-    public static boolean isNumberParity(String yesNo) {
 
-        int rand = 1;
-            if ((rand % 2 == 0)&(yesNo.equals("yes"))) {
-                System.out.println("Correct!");
+    public static String whatIsYourName() {
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        return name;
+    }
+
+    public static boolean playGame(String yN) {
+        System.out.println("Hello, ");
+        double a = Math.random() * 100;
+        int rand = (int) a;
+        for (var i = 0; i < 3; i++) {
+            System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+            System.out.println("Question: " + rand);
+            System.out.println("Your answer: ");
+            if (playGame(yN) == false) {
+                break;
             }
-            if ((rand % 2 != 0)&(yesNo.equals("yes"))) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
-                System.out.println("Let's try again, " + "!");
-                return false;
-        }
-            if ((rand % 2 == 0)&(yesNo.equals("no"))) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
-                System.out.println("Let's try again, "  + "!");
-                return false;
-        } if ((rand % 2 != 0)&yesNo.equals("no")) {
+            if (i == 2) {
+                System.out.println("Congratulations!");
+            }
+            if ((rand % 2 == 0) & (yN.equals("yes"))) {
                 System.out.println("Correct!");
-        } else {
+            } else if ((rand % 2 != 0) & (yN.equals("yes"))) {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.");
+                System.out.println("Let's try again!");
+                return false;
+            } else if ((rand % 2 == 0) & (yN.equals("no"))) {
+                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
+                System.out.println("Let's try again!");
+                return false;
+            } else if ((rand % 2 != 0) & yN.equals("no")) {
+                System.out.println("Correct!");
+            } else {
                 System.out.println("Try again!");
                 return false;
-            } return true;
-    }
-    }
+            }
+        }
+            return true;
+        }
+
+        public static String isNumberParity () {
+            Scanner sc = new Scanner(System.in);
+            String yN = sc.nextLine();
+            return yN;
+        }
+}
+
+//    int rand = 3;
