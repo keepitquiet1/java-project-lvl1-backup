59fe88bb9d9e0334b480ff27fc4b47c753ae63a4
Home <david.klesch@gmail.com>
2022-09-08 20:39:20 +0300

    step 6 8/09/2022

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,10 +1,12 @@
 package hexlet.code;
+import java.util.Random;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.println("Your choice: ");
         Scanner sc = new Scanner(System.in);
@@ -13,13 +15,17 @@ public class App {
             System.out.println("Bye!");
         }
         if (gameNumber == 1) {
+            System.out.println("Welcome to the Brain Games!");
+        }
+        if (gameNumber == 2) {
+            System.out.println("Welcome to the Brain Games!");
             System.out.println("May I have your name?");
             Scanner ss = new Scanner(System.in);
             String name = ss.nextLine();
             System.out.println("Hello, " + name);
             System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
             for (int i = 0; i <= 2; i++) {
-                if (playGame(name) == true) {
+                if (playGame(name)) {
                     if (i == 2) {
                         System.out.println("Congratulations, " + name + "!");
                     }
@@ -28,9 +34,28 @@ public class App {
                 }
             }
         }
+        else if (gameNumber == 3) {
+            System.out.println("Welcome to the Brain Games!");
+            System.out.println("May I have your name?");
+            Scanner ss = new Scanner(System.in);
+            String name = ss.nextLine();
+            System.out.println("Hello, " + name + "!");
+            System.out.println("What is the result of the expression?");
+            for (int i = 0; i <= 2; i++) {
+                if (calculator()) {
+                    if (i == 2) {
+                        System.out.println("Congratulations, " + name + "!");
+                    }
+                } else {
+                    System.out.println("Let's try again, " + name + "!");
+                    break;
+                }
+            }
+        }
     }
     public static boolean playGame(String name) {
-        double a = Math.random() * 100;
+        final int hundred = 100;
+        double a = Math.random() * hundred;
         int rand = (int) a;
         System.out.println("Question: " + rand);
         System.out.println("Your answer: ");
@@ -54,5 +79,51 @@ public class App {
             System.out.println("Try again " + name + "!");
             return false;
         }
+    }
+    public static boolean calculator() {
+        final int hundred = 100;
+        double a = Math.random() * hundred;
+        double b = Math.random() * hundred;
+        String[] plusMinus;
+        plusMinus = new String[3];
+        plusMinus[0] = "*";
+        plusMinus[1] = "+";
+        plusMinus[2] = "-";
+        int f1 = (int) a;
+        int f2 = (int) b;
+        Random z = new Random();
+        int n = z.nextInt(3);
+        System.out.println("Question: " + f1 + plusMinus[n] + f2);
+        System.out.println("Your answer: ");
+        Scanner sf = new Scanner(System.in);
+        int answer = sf.nextInt();
+        if (plusMinus[n].equals("+")) {
+            if (f1+f2==(int)answer) {
+                System.out.println("Correct!");
+            } else {
+                int trueAnswer = f1+f2;
+                System.out.println(answer + " is wrong answer ;(. Correct answer was " + trueAnswer);
+                return false;
+            }
+        }
+        if (plusMinus[n].equals("-")) {
+            if (f1-f2==(int)answer) {
+                System.out.println("Correct!");
+            } else {
+                int trueAnswer = f1-f2;
+                System.out.println(answer + " is wrong answer ;(. Correct answer was " + trueAnswer);
+                return false;
+            }
+        }
+        if (plusMinus[n].equals("*")) {
+            if (f1*f2==(int)answer) {
+                System.out.println("Correct!");
+            } else {
+                int trueAnswer = f1*f2;
+                System.out.println(answer + " is wrong answer ;(. Correct answer was " + trueAnswer);
+                return false;
+            }
+        }
+        return true;
     }
 }
